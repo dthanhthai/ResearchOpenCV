da0491df8d5943a87751ddeec969d0c9e51c8f69
THAIDT\THAI DOAN <dthanhthai09@gmail.com>
2019-12-05 02:06:37 +0700

    add main menu

--- app/src/main/java/com/thaidt/testopencv/MainActivity.java
+++ app/src/main/java/com/thaidt/testopencv/MainActivity.java
@@ -1,14 +1,22 @@
 package com.thaidt.testopencv;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import org.opencv.android.OpenCVLoader;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
     static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
@@ -16,10 +24,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-            if (OpenCVLoader.initDebug()) {
-                Log.i(TAG, "OpenCV initialize success");
+        if (OpenCVLoader.initDebug()) {
+            Toast.makeText(this, "OpenCV initialize success", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "OpenCV initialize failed", Toast.LENGTH_LONG).show();
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+        }
+
+        findViewById(R.id.colorBlobDetectionBtn).setOnClickListener(this);
+        findViewById(R.id.drawHistogramBtn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.colorBlobDetectionBtn:
+                startActivity(new Intent(this, ColorBlodDetectionActivity.class));
+                break;
+            case R.id.drawHistogramBtn:
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
             } else {
-                Log.i(TAG, "OpenCV initialize failed");
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
             }
+        }
     }
 }
+
